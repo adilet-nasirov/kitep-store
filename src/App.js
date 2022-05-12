8ea062365e384071591447cbb70744dc0bd51238
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-05-11 21:57:20 -0700

    improvemetn

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import {useEffect, useState} from 'react'
-import axios from "axios";
-import ApiCall from './Data/ApiCall';
 
 function App() {
   const [data, setData]=useState([])
-  
+  const url=`https://api.nytimes.com/svc/books/v3/lists/full-overview.json?api-key=${process.env.REACT_APP_BOOKS_API_KEY}`;
+  useEffect(()=>{
+    fetch(url).then(responce=> responce.json()).then(books=>setData(books.results.lists));
+  },[])
   return (
     <div className="App">
-      <ApiCall setData={setData}/>
+      {
+        data && data.map((bookList,index)=>{
+          return(<div key={index}>
+            <h2>{bookList.display_name}</h2>
+            {
+              bookList.books.map(book=>{
+                return(<div>
+                  <img src={book.book_image} />
+                </div>)
+              })
+
+          }</div>)
+        })
+      }
     </div>
   );
 }
--- src/Data/ApiCall.js
+++ src/Data/ApiCall.js
@@ -1,14 +1,5 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-const ApiCall = ({setData}) => {
-    const url=`https://api.nytimes.com/svc/books/v3/lists/full-overview.json?api-key=${process.env.REACT_APP_BOOKS_API_KEY}`;
-    useEffect(()=>{
-        const fetchBooks = async()=>{
-            const res =await axios.get(url);
-            setData(res.data.results.lists)
-        }
-        fetchBooks();
-    },[])
+const ApiCall = () => {
+
 }
 
 export default ApiCall
